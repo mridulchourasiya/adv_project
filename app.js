b6ea5c1f550eefdd92a6f03034f12e0124215e40
mridulchourasiya <mridulchourasia11@gmail.com>
2025-07-10 22:20:35 +0530

    middleware for error handling and also models changing

--- app.js
+++ app.js
@@ -5,12 +5,18 @@ import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import connectToDatabase from "./database/mongodb.js";
+import errorMiddleware from "./middleware/error.middleware.js";
+import cookieParser from "cookie-parser";
 
 const app = experss();
+app.use(experss.json());
+app.use(experss.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
+app.use(errorMiddleware);
 
 app.get("/", (req, res) => {
   res.send("<h1>Welcome to the Subscription Management API</h1>");
---
+++ models/subscription.model.js
@@ -0,0 +1,106 @@
+import mongoose from "mongoose";
+
+const subscriptionSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Subscription name is required"],
+      trim: true,
+      minLength: 2,
+      maxLength: 100,
+    },
+    price: {
+      type: Number,
+      required: [true, "subcscription price is required"],
+      min: [0, "Price cannot be negative"],
+    },
+    currency: {
+      type: String,
+      enum: ["USD", "EUR", "GBP", "INR"],
+      default: "INR",
+    },
+
+    frequency: {
+      type: String,
+      enum: ["daily", "weekly", "monthly", "yearly"],
+      required: [true, "Subscription frequency is required"],
+    },
+
+    category: {
+      type: String,
+      required: true,
+      enum: [
+        "sports",
+        "entertainment",
+        "education",
+        "health",
+        "technology",
+        "other",
+      ],
+    },
+
+    paymentMethod: {
+      type: String,
+      required: [true, "Payment method is required"],
+      trim: true,
+    },
+    status: {
+      type: String,
+      enum: ["active", "inactive", "cancelled"],
+      default: "active",
+    },
+
+    startDate: {
+      type: Date,
+      required: true,
+      validate: {
+        validator: (value) => value <= new Date(),
+        message: "Start date cannot be in the future",
+      },
+    },
+    renewalDate: {
+      type: Date,
+      required: true,
+      validate: {
+        validator: function (value) {
+          return value >= this.startDate;
+        },
+        message: "Renewal date must be after the start date",
+      },
+    },
+
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      index: true,
+    },
+  },
+  { timestamps: true }
+);
+
+// auto-calculate renewal date based in frequency
+subscriptionSchema.pre("save", function (next) {
+  if (!this.renewalDate) {
+    const renewalPeriods = {
+      daily: 1,
+      weekly: 7,
+      monthly: 30,
+      yearly: 365,
+    };
+
+    this.renewalDate = new Date(this.startDate);
+    this.renewalDate.setDate(
+      this.renewalDate.getDate() + renewalPeriods[this.frequency]
+    );
+  }
+
+  // auto-update the status if the renewal date has passed
+  if (this.renewalDate < new Date()) {
+    this.status = "exprired";
+  }
+
+  next();
+});
+
+export const Subscription = mongoose.model("Subscription", subscriptionSchema);
